9adaf847be8b0d5eb3c2e6b0a8f32bd268cab32a
Shiki-mori <2296690003@qq.com>
2026-03-20 10:32:36 +0800

    完成md文档说明；保存节点记录

--- frontend/src/api/parseUrl.ts
+++ frontend/src/api/parseUrl.ts
@@ -1,14 +1,23 @@
 // parseUrl.js
-export const parseGachaUrl = (fullUrl) => {
+export const parseGachaUrl = (fullUrl: string): {
+    serverId: string | null;
+    playerId: string | null;
+    cardPoolId: string | null;
+    cardPoolType: number | null;
+    languageCode: string | null;
+    recordId: string | null;
+} | null => {
     try {
         const urlObj = new URL(fullUrl.replace('#/record', '')); // 处理掉 hash 部分方便解析
         const params = urlObj.searchParams;
+
+        const gachaType = params.get('gacha_type');
 
         return {
             serverId: params.get('svr_id'),
             playerId: params.get('player_id'),
             cardPoolId: params.get('resources_id'), // 注意这里：url里叫resources_id，API里叫cardPoolId
-            cardPoolType: parseInt(params.get('gacha_type')),
+            cardPoolType: gachaType ? parseInt(gachaType, 10) : null,
             languageCode: params.get('lang'),
             recordId: params.get('record_id')
         };
