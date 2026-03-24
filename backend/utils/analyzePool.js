51cb41805f6a9ea74ae7a03aebf8f5f2dbb172ef
Shiki-mori <2296690003@qq.com>
2026-03-24 15:25:43 +0800

    实现部分高级计算功能

--- backend/utils/analyzePool.js
+++ backend/utils/analyzePool.js
@@ -1,12 +1,11 @@
 //分析单个卡池中的数据
 
-const {isUpItem}=require('./isUpItem')
+const { isUpItem } = require('./isUpItem')
 
 function analyzePool(records) {
     //const theTotal = records.data.length;
     const theTotal = records.length;//设定参数为纯数组，优化接口结构
     let fiveStarCount = 0;
-    let fourStarCount = 0;
 
     // for (const item of records.data) {
     //     if (item.qualityLevel === 5) {
@@ -18,16 +17,38 @@ function analyzePool(records) {
 
     let pity = 0;//当前总抽数
     let pityList = [];//记录每次出金的抽数（到当前抽数）
+    let upCount = 0;//记录限定数量
 
-    records = records.reverse();
+    let upSeries = 0;//记录连续up次数
+    let notupSeries = 0;//记录连歪次数
+    let upSeriesList = [];//记录连续up次数列表
+    let notupSeriesList = [];//记录连歪次数列表
+
+    //records = records.reverse();  //reverse()会直接修改records。如果后续需使用records可能发生危险。
+    records = records.slice().reverse();
 
     for (const record of records) {
         pity++;
 
         if (record.qualityLevel === 5) {
-            
+
             fiveStarCount++;
+
             const isUp = isUpItem(record.resourceId);
+            if (isUp === true) {
+                upCount++;
+                upSeries++;
+                if (notupSeries > 0) {
+                    notupSeriesList.push(notupSeries);
+                    notupSeries = 0;
+                } else {
+                    notupSeries++;
+                    if (upSeries > 0) {
+                        upSeriesList.push(upSeries);
+                        upSeries = 0;
+                    }
+                }
+            }
 
             pityList.push({
                 count: pity,
@@ -37,23 +58,29 @@ function analyzePool(records) {
 
             pity = 0; // 重置出金抽数，新一轮保底开始
         }
-        else if (record.qualityLevel === 4) {
-            fourStarCount++;
-        }
     }
 
+
+    upSeriesList.push(upSeries);//仅在up状态转换时记录。因此需要在循环结束后添加。
+    notupSeriesList.push(notupSeries);
+
     const avgPity = pityList.length ? (pityList.reduce((sum, item) => sum + item.count, 0) / pityList.length) : 0;
+    const avgUp = upCount ? ((theTotal - pity) / upCount) : 0;
+    const upRate = fiveStarCount ? (upCount / fiveStarCount) : 0;
 
     return {
-        total: theTotal,
+        total: theTotal,//总抽数
         fiveStar: fiveStarCount,
-        fourStar: fourStarCount,
-        avgPity: avgPity,
+        avgPity: avgPity.toFixed(2),//平均出金抽数
+        avgUp: avgUp.toFixed(2),//平均up抽数
+        upRate: upRate.toFixed(4),//up率
         pityList,
-        "已垫": pity
+        "已垫": pity,
+        upSeries: upSeriesList.length ? Math.max(...upSeriesList) : 0,
+        notupSeries: notupSeriesList.length ? Math.max(...notupSeriesList) : 0
     };
 }
 
 module.exports = {
     analyzePool
-};
+}
