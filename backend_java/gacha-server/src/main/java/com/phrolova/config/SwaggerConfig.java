25fe88324347d46ccb31d1724ee33a01be7eb766
Shiki-mori <2296690003@qq.com>
2026-05-25 17:41:23 +0800

    ``` feat(config): 更新Swagger配置以使用OpenAPI 3
    移除过时的Swagger 2配置并迁移到OpenAPI 3规范，
    简化了配置类结构并更新了API文档生成方式。
    swagger仍无法使用
    ```

--- backend_java/gacha-server/src/main/java/com/phrolova/config/SwaggerConfig.java
+++ backend_java/gacha-server/src/main/java/com/phrolova/config/SwaggerConfig.java
@@ -14,38 +14,13 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-// @EnableOpenApi
-public class SwaggerConfig extends WebMvcConfigurationSupport {
-    /**
-     * 通过knife4j生成接口文档
-     * @return Docket对象，用于Swagger文档生成
-     */
-    @Bean// 将docket方法的返回值(Docket对象，用于swagger文档生成)注册为 Spring 容器中的 Bean 实例
-    public Docket docket() {
-        // log.info("Generating Interface Documentation...");
-        ApiInfo apiInfo = new ApiInfoBuilder()
-                .title("Gachaanalyzer Interface Documentation")
-                .version("1.0")
-                .description("抽卡分析接口文档")
-                .build();
-        Docket docket = new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo)
-                .select()
-                // 指定要扫描的包
-                .apis(RequestHandlerSelectors.basePackage("com.phrolova.gachaanlyzer.controller"))
-                .paths(PathSelectors.any())
-                .build();
-        return docket;
-    }
-
-    /**
-     * 设置静态资源映射
-     * @param registry
-     */
-    @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // log.info("设置静态资源映射...");
-        registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
-        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI gachaOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Gachaanalyzer Interface Documentation")
+                        .version("1.0")
+                        .description("抽卡分析接口文档"));
     }
 }
